0287f2f1b060859e6c76314ceccad5db002fc245
Renuka Manavalan <remanava@microsoft.com>
2023-01-20 00:31:59 +0000

    Made to lib

--- src/lib/subdir.mk
+++ src/lib/subdir.mk
@@ -1,6 +1,5 @@
 CC := g++
 
-TEST_OBJS += ./lib/common.o ./lib/lom_lib.o ./lib/transport.o
 OBJS += ./lib/common.o ./lib/lom_lib.o ./lib/transport.o
 
 C_DEPS += ./lib/common.d ./lib/lom_lib.d ./lib/transport.d
@@ -11,3 +10,4 @@ lib/%.o: lib/%.cpp
 	$(CC) -D__FILENAME__="$(subst lib/,,$<)" -I../common $(CFLAGS) -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
+
