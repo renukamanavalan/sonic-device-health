c2133f0b2e5852e17ebd16a896feb5efd2856c16
Renuka Manavalan <remanava@microsoft.com>
2023-04-28 20:17:27 +0000

    Create vcache dir

--- docker-build/build.sh
+++ docker-build/build.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+/* Required by docker build */
+mkdir -p lom-slave-bullseye/vcache
+
 docker inspect --type image lom-slave-bullseye:1234 &> /dev/null; RET=$?
 if [[ ${RET} -ne 0 ]];then
     docker build --no-cache -t lom-slave-bullseye:1234 --build-arg http_proxy= --build-arg https_proxy= --build-arg no_proxy= --build-arg SONIC_VERSION_CACHE= --build-arg SONIC_VERSION_CONTROL_COMPONENTS=none lom-slave-bullseye 2>&1 | tee lom-slave-bullseye/lom-slave-bullseye.log
