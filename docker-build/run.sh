17f04ae4db8cd5436eba7d1ce8cb9e8d52462ba3
Renuka Manavalan <remanava@microsoft.com>
2023-05-01 23:29:39 +0000

    Build environment

---
+++ docker-build/run.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+set -x
+
+docker run --rm=true --privileged --init \
+    -v "$(dirname ${PWD}):/lom-root" \
+    -v "/tmp/docklock:/tmp/docklock"\
+    -w "/lom-root" -e "http_proxy=" -e "https_proxy=" -e "no_proxy=" -it \
+    lom-slave-bullseye-admin:1234
