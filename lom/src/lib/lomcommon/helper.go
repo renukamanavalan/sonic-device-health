69d17aa2752bfd9ea198ab2d1fcdac19fa5cbe4c
Renuka Manavalan <47282725+renukamanavalan@users.noreply.github.com>
2023-06-19 15:34:24 -0700

    Build debian package in SONiC build environment (#26)
    Build Debian package successfully to enable install into SONiC container

--- lom/src/lib/lomcommon/helper.go
+++ lom/src/lib/lomcommon/helper.go
@@ -3,8 +3,9 @@ package lomcommon
 import (
     "encoding/json"
     "errors"
+    "flag"
     "fmt"
-    "log"
+    "io"
     "log/syslog"
     "math"
     "os"
@@ -17,7 +18,12 @@ import (
     "time"
 )
 
-var writers = make(map[syslog.Priority]*syslog.Writer)
+/* "go test" must use "-v" option to turn on testmode */
+var RunningTestMode = false
+var logSuffix = ""
+
+var writers = make(map[syslog.Priority]io.Writer)
+var logInitDone = false
 
 var log_level = syslog.LOG_DEBUG
 
@@ -31,11 +37,36 @@ func RunPanic(m string) {
 var DoPanic = RunPanic
 
 func init() {
+    for i := syslog.LOG_EMERG; i <= syslog.LOG_DEBUG; i++ {
+        writers[i] = os.Stderr
+    }
+    logSuffix = "\n"
+}
+
+func LogInit() {
+    if logInitDone {
+        return
+    }
+
+    if !flag.Parsed() {
+        flag.Parse()
+    }
+
+    /* "-v" flag passed via "go test" sets this "test.v" flag */
+    RunningTestMode = flag.Lookup("test.v") != nil
+    if RunningTestMode {
+        fmt.Fprintf(writers[syslog.LOG_ERR], "RunningTestMode=(%v)\n", RunningTestMode)
+        /* TestMode use stderr as done in init */
+        return
+    }
+    /* No need for newline with syslog */
+    logSuffix = ""
 
     for i := syslog.LOG_EMERG; i <= syslog.LOG_DEBUG; i++ {
         writer, err := syslog.Dial("", "", (i | syslog.LOG_LOCAL7), "")
         if err != nil {
-            log.Fatal(err)
+            fmt.Fprintf(os.Stderr, "Failed to get syslog writer. Exiting ...\n")
+            OSExit(-1)
         }
         writers[i] = writer
     }
@@ -45,7 +76,9 @@ func init() {
      *  fmt.Fprintf(writers[syslog.LOG_WARNING], "This is a daemon warning message")
      *  fmt.Fprintf(writers[syslog.LOG_ERR], "This is a daemon ERROR message")
      *  fmt.Fprintf(writers[syslog.LOG_INFO], "This is a daemon INFO message")
+     *
      */
+    logInitDone = true
 }
 
 /* Return currently set log level */
@@ -112,7 +145,7 @@ func LogMessageWithSkip(skip int, lvl syslog.Priority, s string, a ...interface{
     ct_lvl := GetLogLevel()
     m := fmt.Sprintf(getPrefix(skip+2)+s, a...)
     if lvl <= ct_lvl {
-        FmtFprintf(writers[lvl], m)
+        FmtFprintf(writers[lvl], m+logSuffix)
         if ct_lvl >= syslog.LOG_DEBUG {
             /* Debug messages gets printed out to STDOUT */
             fmt.Printf(m)
