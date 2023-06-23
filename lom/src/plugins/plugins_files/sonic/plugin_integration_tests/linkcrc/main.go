73b5fcaea38fbfbbed8f8e5cacf47d4597008ab4
prithviraghavtm <39837936+prithviraghavtm@users.noreply.github.com>
2023-06-22 23:11:48 -0400

    Link Crc Mocker (#27)
    * Link Crc Simulator
    ---------
    
    Co-authored-by: Prithvi <ptallapallim@microsoft.com>

--- lom/src/plugins/plugins_files/sonic/plugin_integration_tests/linkcrc/main.go
+++ lom/src/plugins/plugins_files/sonic/plugin_integration_tests/linkcrc/main.go
@@ -2,20 +2,18 @@ package main
 
 import (
     "context"
-    "fmt"
-    "io/ioutil"
+    "github.com/go-redis/redis"
     "lom/src/lib/lomcommon"
     "lom/src/lib/lomipc"
     "lom/src/plugins/plugins_common"
     "lom/src/plugins/plugins_files/sonic/plugin/linkcrc"
+    "lom/src/plugins/plugins_files/sonic/plugin_integration_tests/linkcrc_mocker/linkcrc_utils"
     "lom/src/plugins/plugins_files/sonic/plugin_integration_tests/utils"
     "os"
     "os/exec"
-    "strconv"
     "strings"
     "time"
-
-    "github.com/go-redis/redis"
+    "encoding/json"
 )
 
 const (
@@ -45,18 +43,32 @@ func main() {
         utils.PrintInfo("Successfuly Disabled counterpoll")
     }
 
-    // Perform Actual integration test
-    ctx, cancelFunc := context.WithCancel(context.Background())
-    go InvokeLinkCrcDetectionPlugin(cancelFunc)
-loop:
-    for {
-        select {
-        case <-time.After(3 * time.Minute):
-            utils.PrintError("Timeout. Aborting Integration test")
-            break loop
-        case <-ctx.Done():
-            break loop
+    outliersArray := []string{"1,0,0,0", "1,0,1,0,0", "1,0,0,0,0"}
+    shouldDetectCrc := []bool{true, true, false}
+
+    for index := 0; index < len(outliersArray); index++ {
+        ctx, cancelFunc := context.WithCancel(context.Background())
+        shouldDetect := shouldDetectCrc[index]
+        go InvokeLinkCrcDetectionPlugin(outliersArray[index], ctx, cancelFunc, shouldDetect)
+        timeoutTimer := time.NewTimer(time.Duration(3) * time.Minute)
+    loop:
+        for {
+            select {
+            case <-timeoutTimer.C:
+                if shouldDetect {
+                    utils.PrintError("Timeout. Aborting Integration test")
+                } else {
+                    utils.PrintInfo("Integration Test Succeeded as timeout was expected")
+                }
+                break loop
+            case <-ctx.Done():
+                if !shouldDetect {
+                    utils.PrintError("Integration Test Failed")
+                }
+                break loop
+            }
         }
+        timeoutTimer.Stop()
     }
 
     // Post - clean up
@@ -69,10 +81,11 @@ loop:
     utils.PrintInfo("Its exepcted not to receive any heartbeat or plugin logs from now as the anomaly is detected")
 }
 
-func InvokeLinkCrcDetectionPlugin(cancelFunc context.CancelFunc) {
-    go MockRedisData()
+func InvokeLinkCrcDetectionPlugin(pattern string, ctx context.Context, cancelFunc context.CancelFunc, shouldDetect bool) {
+    go linkcrc_utils.MockRedisWithLinkCrcCounters(pattern, 10, os.Args[1:], ctx)
     linkCrcDetectionPlugin := linkcrc.LinkCRCDetectionPlugin{}
-    actionCfg := lomcommon.ActionCfg_t{Name: action_name, Type: detection_type, Timeout: 0, HeartbeatInt: 10, Disable: false, Mimic: false, ActionKnobs: ""}
+    actionKnobs := json.RawMessage(``)
+    actionCfg := lomcommon.ActionCfg_t{Name: action_name, Type: detection_type, Timeout: 0, HeartbeatInt: 10, Disable: false, Mimic: false, ActionKnobs: actionKnobs}
     linkCrcDetectionPlugin.Init(&actionCfg)
     actionRequest := lomipc.ActionRequestData{Action: action_name, InstanceId: "InstId", AnomalyInstanceId: "AnInstId", AnomalyKey: "", Timeout: 0}
     pluginHBChan := make(chan plugins_common.PluginHeartBeat, 10)
@@ -80,30 +93,20 @@ func InvokeLinkCrcDetectionPlugin(cancelFunc context.CancelFunc) {
     time.Sleep(10 * time.Second)
     response := linkCrcDetectionPlugin.Request(pluginHBChan, &actionRequest)
     utils.PrintInfo("Integration testing Done.Anomaly detection result: %s", response.AnomalyKey)
-    for _, v := range os.Args[1:] {
-        if !strings.Contains(response.AnomalyKey, v) {
-            utils.PrintError("Integration Test Failed")
-            cancelFunc()
-            return
+    if shouldDetect {
+        for _, v := range os.Args[1:] {
+            if !strings.Contains(response.AnomalyKey, v) {
+                utils.PrintError("Integration Test Failed")
+                cancelFunc()
+                return
+            }
         }
+        utils.PrintInfo("Integration Test Succeeded")
     }
-    utils.PrintInfo("Integration Test Succeeded")
     cancelFunc()
 }
 
-func MockRedisData() error {
-    datapoints := make([]map[string]interface{}, 5)
-
-    for index := 0; index < 5; index++ {
-        countersForLinkCRCBytes, err := ioutil.ReadFile(fileName + strconv.Itoa(index+1) + ".txt")
-        if err != nil {
-            utils.PrintError("Error reading file %d. Err %v", index+1, err)
-            return err
-        }
-        datapoints[index] = utils.LoadConfigToMap(countersForLinkCRCBytes)
-        fmt.Println(datapoints[index])
-    }
-
+func MockRedisData(outliers []int) error {
     var countersDbClient = redis.NewClient(&redis.Options{
         Addr:     redis_address,
         Password: redis_password,
@@ -141,11 +144,46 @@ func MockRedisData() error {
         }
     }
 
-    // Mock counters with sleep.
+    // Write first data points into redis.
     utils.PrintInfo("Counters Mock Initiated")
-    for datapointIndex := 0; datapointIndex < len(datapoints); datapointIndex++ {
+    var ifInErrors float64
+    var ifInUnicastPackets float64
+    var ifOutUnicastPackets float64
+    var ifOutErrors float64
+    ifInErrors = 100
+    ifInUnicastPackets = 101
+    ifOutUnicastPackets = 1100
+    ifOutErrors = 1
+    datapoint := map[string]interface{}{"SAI_PORT_STAT_IF_IN_ERRORS": ifInErrors, "SAI_PORT_STAT_IF_IN_UCAST_PKTS": ifInUnicastPackets, "SAI_PORT_STAT_IF_OUT_UCAST_PKTS": ifOutUnicastPackets, "SAI_PORT_STAT_IF_OUT_ERRORS": ifOutErrors}
+    for ifName, oidMapping := range mockedLinks {
+        _, err := countersDbClient.HMSet(counters_db+oidMapping, datapoint).Result()
+        if err != nil {
+            utils.PrintError("Error mocking redis data for interface %s. Err %v", ifName, err)
+            return err
+        } else {
+            utils.PrintInfo("Successfuly mocked redis data for interface %s", ifName)
+        }
+    }
+    time.Sleep(30 * time.Second)
+
+    // Mock counters with sleep.
+    for outlier := 0; outlier < len(outliers); outlier++ {
+        if outliers[outlier] == 1 {
+            ifInErrors = ifInErrors + 600
+            ifInUnicastPackets = ifInUnicastPackets + 1005
+            ifOutUnicastPackets = ifOutUnicastPackets + 1005
+            ifOutErrors = ifOutErrors + 30
+        } else {
+            ifInErrors = ifInErrors + 200
+            ifInUnicastPackets = ifInUnicastPackets + 300000015
+            ifOutUnicastPackets = ifOutUnicastPackets + 1005
+            ifOutErrors = ifOutErrors + 30
+        }
+
+        datapoint = map[string]interface{}{"SAI_PORT_STAT_IF_IN_ERRORS": ifInErrors, "SAI_PORT_STAT_IF_IN_UCAST_PKTS": ifInUnicastPackets, "SAI_PORT_STAT_IF_OUT_UCAST_PKTS": ifOutUnicastPackets, "SAI_PORT_STAT_IF_OUT_ERRORS": ifOutErrors}
+
         for ifName, oidMapping := range mockedLinks {
-            _, err := countersDbClient.HMSet(counters_db+oidMapping, datapoints[datapointIndex]).Result()
+            _, err := countersDbClient.HMSet(counters_db+oidMapping, datapoint).Result()
             if err != nil {
                 utils.PrintError("Error mocking redis data for interface %s. Err %v", ifName, err)
                 return err
---
+++ lom/src/plugins/plugins_files/sonic/plugin_integration_tests/linkcrc_mocker/README.md
@@ -0,0 +1,20 @@
+Steps to execute below test cases for LoM integration tests:
+
+1. Navigate to this folder lom/src/plugins/sonic/plugin_integration_tests/linkcrc_mocker  
+2. Execute "go build ."
+3. Copy the generated binary linkcrc_mocker to star lab switch.
+4. Execute the binary with below TestId as first arugment followed by space seperated list of interfaces. For ex, ./linkcrc_mocker 1 Ethernet96 Ethernet100
+
+
+
++--------+-----------------------------------+-----------+----------------------------------------------+----------------------+---------------------+
+| TestId | Description                       | Detection | Max time out for integration test in seconds | Mock run for period  | Pattern of outliers |
++--------+-----------------------------------+-----------+----------------------------------------------+----------------------+---------------------+
+| 0      | Continuous Outliers               | True      | 60 for first detection                       | 10 minutes           | 1                   |
++--------+-----------------------------------+-----------+----------------------------------------------+----------------------+---------------------+
+| 1      | No Outliers                       | False     | No detection at any time                     | 10 minutes           | 0                   |
++--------+-----------------------------------+-----------+----------------------------------------------+----------------------+---------------------+
+| 2      | Only first and last outlier       | True      | 240 for first detection                      | 10 minutes           | 1,0,0,0             |
++--------+-----------------------------------+-----------+----------------------------------------------+----------------------+---------------------+
+| 3      | Only one outliers in 5 iterations | False     | No detection at any time                     | 10 minutes           | 1,0,0,0,0           |
++--------+-----------------------------------+-----------+----------------------------------------------+----------------------+---------------------+
