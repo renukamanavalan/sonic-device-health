b211da4dce4a35be8c83c091f5a7b22e662f021d
Renuka Manavalan <remanava@microsoft.com>
2023-09-27 23:54:46 +0000

    Added as 2 consecutive req/rep

--- lom/src/lib/libtest/telemetry_data_reqrep.go
+++ lom/src/lib/libtest/telemetry_data_reqrep.go
@@ -67,6 +67,53 @@ var pubSubReqRepSuite = testSuite_t{
             "read from sub channel created above",
         },
         testEntry_t{
+            script.ApiIDSendClientRequest,
+            []script.Param_t{
+                script.Param_t{"chType_1", nil, nil}, /* Fetch chType_1 from cache */
+                script.Param_t{"req_1", tele.ClientReq_t("request:Hello Mars"), nil},
+            },
+            []result_t{
+                result_t{"chClientRes-0", nil, validateNonNil}, /* chan to read response */
+                NIL_ERROR,
+            },
+            "Send a request as if from client",
+        },
+        /* Simulate Server read req and respond */
+        testEntry_t{
+            script.ApiIDReadClientRequest,                  /* Server read req */
+            []script.Param_t{
+                script.Param_t{"chSerReq-0", nil, nil},     /* Get chRead_0 from cache */
+                script.Param_t{script.ANONYMOUS, 1, nil},   /* timeout = 1 second */
+            },
+            []result_t{
+                result_t{"req_1", nil, nil}, /* Validate against cache val for req_1 */
+                NIL_ERROR,
+            },
+            "As server read your request",
+        },
+        testEntry_t{
+            script.ApiIDSendClientResponse,                 /* Server Write response */
+            []script.Param_t{
+                script.Param_t{"chSerRes-0", nil, nil},     /* Use chan from cache */
+                script.Param_t{"res_1", tele.ServerRes_t("resp: Hi Mars!"), nil},   /* Save written data in cache */
+                script.Param_t{script.ANONYMOUS, 1, nil},   /* timeout = 1 second */
+            },
+            []result_t{ NIL_ERROR },
+            "As server write your response",
+        },
+        testEntry_t{
+            script.ApiIDReadClientResponse,                 /* Client read its response */
+            []script.Param_t{
+                script.Param_t{"chClientRes-0", nil, nil},  /* Get chan from cache */
+                script.Param_t{script.ANONYMOUS, 1, nil},   /* timeout = 1 second */
+            },
+            []result_t{
+                result_t{"res_1", nil, nil},                /* Validate against cache val for res_0 */
+                NIL_ERROR,
+            },
+            "read from sub channel created above",
+        },
+        testEntry_t{
             script.ApiIDCloseChannel,
             []script.Param_t{
                 script.Param_t{"chSerRes-0", nil, nil},     /* Close server handler */
